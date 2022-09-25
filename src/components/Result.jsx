ab4c3dfffb5ed2bb47024c02b25365c58ca54a4b
Mer7z <manuelramirezuma@gmail.com>
2022-09-25 17:55:29 -0500

    Solved html specials characters

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 function Result({siteLink, subject, quote, replies, imageReplies, board, author, img}) {
-  let quoteSub = quote.length > 50 ? quote.substring(0, 50) + '...' : quote
-  quoteSub = quoteSub.replace('<span class="quote">&gt;', '>')
-  quoteSub = quoteSub.replace('</span>', '')
-  quoteSub = quoteSub.replace('&gt;', '>')
+  let quoteSub = quote
+  quoteSub = quoteSub.replace('<span class="quote">', '')
+  quoteSub = quoteSub.replace('</span>')
+  quoteSub = quoteSub.length > 50 ? quoteSub.substring(0, 50) + '...' : quoteSub
+  let sub = subject ? subject + ' - ' : ''
+  const header = `${sub}${quoteSub} - /${board}/`
   return (
     <div className='result-card'>
       <a href={siteLink} target='_blank' className='thread-link' >
@@ -13,7 +15,7 @@ function Result({siteLink, subject, quote, replies, imageReplies, board, author,
       <div className='result-flex d-flex'>
         <div className='result-info-container'>
           <a href={siteLink} target='_blank'>
-            <h3 className='result-title'>{subject ? subject + ' -' : null} {quoteSub} - /{board}/</h3>
+            <h3 dangerouslySetInnerHTML={{__html: header}} className='result-title'></h3>
           </a>
           <p className='result-author'>{author}</p>
           <p className='result-info'>Replies: {replies} | Images: {imageReplies}</p>
